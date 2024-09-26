39b0eb8a4774eeb5220e64c37206fcbcdf655264
ayman <50217271+TheOrca-sol@users.noreply.github.com>
2024-09-26 04:23:59 +0100

    Update OpenPositionsTable.js

--- src/subcomponents/OpenPositionsTable.js
+++ src/subcomponents/OpenPositionsTable.js
@@ -285,7 +285,7 @@ function OpenPositionsTable({ openPositions, sellToken, connection }) {
                   <Typography>Followers: {twitterAnalytics.followerCount || 'N/A'}</Typography>
                   <Typography>Following: {twitterAnalytics.followingCount || 'N/A'}</Typography>
                   <Typography>Tweet Count: {twitterAnalytics.tweetCount || 'N/A'}</Typography>
-                  <Typography>Account Age (days): {twitterAnalytics.accountAge || 'N/A'}</Typography>
+                  <Typography>Account Age (days): {twitterAnalytics.accountAge ? twitterAnalytics.accountAge.toFixed(2) : 'N/A'}</Typography>
                 </>
               )}
               
